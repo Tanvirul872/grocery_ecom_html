66e906ce1b77c92e9d62678d31badccb8666bee8
Tanvirul872 <64800533+Tanvirul872@users.noreply.github.com>
2023-03-04 10:07:42 +0600

    update

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,30 @@ window.onscroll = () =>{
 
     }
 }
+
+let countDate = new Date('april 3, 2023 00:00:00').getTime(); 
+
+function CountDown(){
+
+    let now = new Date().getTime(); 
+    gap = countDate.now; 
+
+    let second = 1000;
+    let minitue = second * 60;
+    let hour = minitue * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap/(day)); 
+    let h = Math.floor(gap % (day)/(hour)); 
+    let m = Math.floor(gap % (hour)/(minitue)); 
+    let s = Math.floor(gap % (minitue)/(second)); 
+
+    document.getElementById('day').innerText = d ;
+    document.getElementById('hour').innerText = h ;
+    document.getElementById('miniute').innerText = m ;
+    document.getElementById('second').innerText = s ;
+
+}
+setInterval(function(){
+   CountDown() ; 
+},1000);
